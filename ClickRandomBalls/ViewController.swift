9b83e91980bfe3b9ec7a5cb50b545cb6425c1a7c
Xu4xOK <alexnovalabs@gmail.com>
2016-12-05 18:36:43 -0800

    Initial Commit

---
+++ ClickRandomBalls/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClickRandomBalls
+//
+//  Created by Alex Novos on 12/5/16.
+//  Copyright © 2016 Alex Novos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
